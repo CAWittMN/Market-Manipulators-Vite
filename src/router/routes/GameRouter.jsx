3079b9153c82073247d6ef21e60603ecfee83684
Casey Wittrock <caseywittrockmn@gmail.com>
2024-10-26 19:04:14 -0500

    change game router

--- src/router/routes/GameRouter.jsx
+++ src/router/routes/GameRouter.jsx
@@ -9,18 +9,14 @@ import MarketTransition from "../../components/game/transitionPhase/MarketTransi
 import MarketPage from "../../components/game/marketPhase/MarketPage";
 
 const GameRouter = () => {
-  const { currGame } = useContext(AppContext);
   return (
     <Routes>
-      {currGame && (
-        <>
-          <Route path="/" element={<MonthTransition />} />
-          <Route path="/rewards" element={<RewardsPage />} />
-          <Route path="/manipulation" element={<ManipulationPage />} />
-          <Route path="/newMarket" element={<MarketTransition />} />
-          <Route path="/market" element={<MarketPage />} />
-        </>
-      )}
+      <Route path="/" element={<MonthTransition />} />
+      <Route path="/rewards" element={<RewardsPage />} />
+      <Route path="/manipulation" element={<ManipulationPage />} />
+      <Route path="/newMarket" element={<MarketTransition />} />
+      <Route path="/market" element={<MarketPage />} />
+
       <Route path="/*" element={<Navigate to="/" />} />
     </Routes>
   );
